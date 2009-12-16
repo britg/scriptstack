821c1b05ab638f3911dfbdeabbea59f39c7c3e50
britg <brit@britg.com>
2009-12-16 13:39:14 -0600

    code is viewable

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,7 @@ Stack.script.prototype = {
     name: "",
     original_size: "",
     minified_size: "",
+    content: "",
 
     bootstrap: function (params) {
         var _this = this;
@@ -74,6 +75,49 @@ Stack.script.prototype = {
         $.post(endpoint, data, function () {
             cb.apply();
         });
+    },
+
+    toggle_code: function () {
+        var _this = this;
+        var li = $('#' + this.id);
+        var code = li.find('code');
+
+        if(li.find('.scriptCode').is(':visible')) {
+            _this.hide_code();
+        } else {
+            if(!('SyntaxHighlighter' in window)) {
+                $.beautyOfCode.init({
+                    brushes:['JScript'],
+                    ready: function () {
+                        _this.show_code();
+                    }
+                });
+            } else {
+                _this.show_code();
+            }
+        }
+    },
+
+    show_code: function () {
+        var li = $('#' + this.id);
+        var pre = li.find('pre.code');
+        var code = li.find('code');
+
+        if(code.length > 0) {
+            if(code.is(':empty')) {
+                code.html(this.content);
+            }
+
+            pre.beautifyCode('javascript');
+            li.find('.scriptCode').show();
+        } else {
+            li.find('.scriptCode').show();
+        }
+    },
+
+    hide_code: function () {
+        var li = $('#' + this.id);
+        li.find('.scriptCode').hide();
     }
 
 };
