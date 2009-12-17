5210c38e17a0e42db2fdb9a682df80b5faa571d1
britg <brit@britg.com>
2009-12-16 22:05:05 -0600

    use environment variables for mongo credentials

--- models/mongo.rb
+++ models/mongo.rb
@@ -2,7 +2,6 @@ require 'mongo_mapper'
 logger = Logger.new($stdout)
 MongoMapper.connection = Mongo::Connection.new('db.mongohq.com', 27017, :auto_reconnect => true, :logger => logger)
 MongoMapper.database = 'dashpp'
-MongoMapper.database.authenticate('britg', '193170bs')
+MongoMapper.database.authenticate('britg', ENV['mongohq_pass'])
 #MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => logger)
 #MongoMapper.database = 'scriptstack'
-
