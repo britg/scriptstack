4cc652f10e16ac8b367cdf355d5da3b7715243ac
britg <brit@britg.com>
2009-12-18 12:21:01 -0600

    Create tests for homepage and Create a Stack link

--- features/home.feature
+++ features/home.feature
@@ -2,3 +2,8 @@ Feature: view pages
     Scenario: Home page
         Given I am viewing "/"
         Then I should see the homepage
+
+    Scenario: Click on "Create a Stack"
+        Given I am viewing "/"
+        When I click on "Create a Stack"
+        Then I should see the Create a Stack page
--- features/step_definitions/scriptstack_steps.rb
+++ features/step_definitions/scriptstack_steps.rb
@@ -2,6 +2,15 @@ Given /^I am viewing "(.+)"$/ do |url|
     visit(url)
 end
 
+
+When /^I click on "Create a Stack"$/ do
+    click_link 'homeCreateLink'
+end
+
 Then /^I should see the homepage/ do
-    response_body.should =~ /(.+)/
+    response_body.should have_selector('h1', :class=>"lead")
+end
+
+Then /^I should see the Create a Stack page$/ do
+    response_body.should have_selector("h2", :class=>"unedited")
 end
